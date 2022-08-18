26d9b1e0d3854134247d6732f4433b913eac7e4a
unknown <scboyhz@gmail.com>
2022-08-19 00:12:03 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('.hamburger') 
+console.log(menu)
+
+
+menu.addEventListener('click', () => {
+    document.querySelector('.nav').classList.toggle('toggle')
+})
+    
